5485570213608926790b7a196875902f0cb4c0df
William Knight <William.Jeffery.Knight@protonmail.com>
2022-11-25 08:17:04 -0500

    created a simple web page

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('Here are our projects')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("projects/", projects, name="projects")
 ]
