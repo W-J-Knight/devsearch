cc272bd06e3da3da8d738e0da25bc227febbb9fe
William Knight <William.Jeffery.Knight@protonmail.com>
2022-11-25 08:25:17 -0500

    created a second simple web page

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -20,7 +20,11 @@ from django.http import HttpResponse
 def projects(request):
     return HttpResponse('Here are our projects')
 
+def project(request):
+    return HttpResponse('SINGLE PRODUCT')
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("projects/", projects, name="projects")
+    path("projects/", projects, name="projects"), 
+    path("project/", project, name="project"), 
 ]
