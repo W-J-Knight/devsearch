7c9047e8cd444d27e9e1ce2a10c0a888faf3b26f
William Knight <William.Jeffery.Knight@protonmail.com>
2022-11-29 09:42:35 -0500

    Render out some data on web page from the data base.

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 projectsList = [
@@ -12,18 +13,12 @@ projectsList = [
 ]
 
 def projects(request):
-    page = 'projects'
-    number = 10 
-    context = {
-        'page':page,
-        'number': number, 
-        'projects': projectsList
+    projects = Project.objects.all()
+    context = { 
+        'projects': projects
     }
     return render(request, 'projects/projects.html',context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html',{'project': projectObj})
